5a729b7f3092ae9e1beb06b4e949762687351261
Ricardo Wurmus <ricardo.wurmus@mdc-berlin.de>
2015-04-21 16:50:13 +0200

    turn dorina.run into a class

--- dorina/run.py
+++ dorina/run.py
@@ -4,124 +4,125 @@ import os
 import logging
 from os import path
 from pybedtools import BedTool
+
 from dorina import utils
 
+class Dorina:
+    def __init__(self, datadir):
+        self.utils = utils.DorinaUtils(datadir)
 
-def analyse(genome, set_a, match_a='any', region_a='any',
-            set_b=None, match_b='any', region_b='any',
-            combine='or', genes=None, window_a=-1, window_b=-1,
-            datadir=None):
-    """Run doRiNA analysis"""
-    logging.debug("analyse(%r, %r(%s) <-'%s'-> %r(%s))" % (genome, set_a, match_a, combine, set_b, match_b))
+    def analyse(self, genome, set_a, match_a='any', region_a='any',
+                set_b=None, match_b='any', region_b='any',
+                combine='or', genes=None, window_a=-1, window_b=-1):
+        """Run doRiNA analysis"""
+        logging.debug("analyse(%r, %r(%s) <-'%s'-> %r(%s))" % (genome, set_a, match_a, combine, set_b, match_b))
 
-    def compute_result(region, regulators, match, window):
-        genome_bed = _get_genome_bedtool(genome, region, datadir, genes)
+        def compute_result(region, regulators, match, window):
+            genome_bed = self._get_genome_bedtool(genome, region, genes)
 
-        # create local copy so we can mangle it
-        _regulators = regulators[:]
-        if window > -1:
-            initial = _regulators.pop(0)
-            genome_bed = genome_bed.intersect(initial)
-            if window > 0:
-                genome_bed = _add_slop(genome_bed, genome, window, datadir)
-
-
-        result = None
-        if match == 'any':
-            result = genome_bed.intersect(_merge_regulators(_regulators), wa=True, u=True)
-        elif match == 'all':
-            results = [genome_bed]
-            results.extend(_regulators)
-            result = reduce(lambda acc, x: acc.intersect(x, wa=True, u=True), results)
-
-        return result
-
-    regulators_a = map(lambda x: _get_regulator_bedtool(x, datadir), set_a)
-    regulators_b = []
-
-    result_a = compute_result(region_a, regulators_a, match_a, window_a)
-    if set_b is not None:
-        regulators_b = map(lambda x: _get_regulator_bedtool(x, datadir), set_b)
-        result_b = compute_result(region_b, regulators_b, match_b, window_b)
-        if combine == 'or':
-            final_results = _merge_regulators([result_a, result_b])
-        elif combine == 'and':
-            final_results = result_a.intersect(result_b, wa=True, u=True)
-        elif combine == 'xor':
-            not_in_b = result_a.intersect(result_b, v=True, wa=True)
-            not_in_a = result_b.intersect(result_a, v=True, wa=True)
-            final_results = _merge_regulators([not_in_b, not_in_a])
-        elif combine == 'not':
-            final_results = result_a.intersect(result_b, v=True, wa=True)
-
-    else:
-        final_results = result_a
-
-    regulators_a.extend(regulators_b)
-    regulator = _merge_regulators(regulators_a)
-    final_results = final_results.intersect(regulator, wa=True, wb=True)
-    return final_results
-
-def _merge_regulators(regulators):
-    """Merge a list of regulators using BedTool.cat"""
-    regulator = regulators[0]
-    for i in range(1, len(regulators)):
-        logging.debug('merging regulator %r' % regulators[i])
-        regulator = regulator.cat(regulators[i], postmerge=False)
-
-    return regulator
+            # create local copy so we can mangle it
+            _regulators = regulators[:]
+            if window > -1:
+                initial = _regulators.pop(0)
+                genome_bed = genome_bed.intersect(initial)
+                if window > 0:
+                    genome_bed = self._add_slop(genome_bed, genome, window)
 
 
-def _add_slop(feature, genome_name, slop, datadir=None):
-    """Add specified slop before and after a regulator"""
-    return feature.slop(g=_get_genome_chromfile(genome_name, datadir), b=slop)
+            result = None
+            if match == 'any':
+                result = genome_bed.intersect(self._merge_regulators(_regulators), wa=True, u=True)
+            elif match == 'all':
+                results = [genome_bed]
+                results.extend(_regulators)
+                result = reduce(lambda acc, x: acc.intersect(x, wa=True, u=True), results)
 
+            return result
 
-def _get_genome_chromfile(genome_name, datadir=None):
-    """Get the path to the .genome file listing chromosome sizes"""
-    return path.join(utils.get_genome_by_name(genome_name),
-                     "{}.genome".format(genome_name))
+        regulators_a = map(lambda x: self._get_regulator_bedtool(x), set_a)
+        regulators_b = []
 
+        result_a = compute_result(region_a, regulators_a, match_a, window_a)
+        if set_b is not None:
+            regulators_b = map(lambda x: self._get_regulator_bedtool(x), set_b)
+            result_b = compute_result(region_b, regulators_b, match_b, window_b)
+            if combine == 'or':
+                final_results = self._merge_regulators([result_a, result_b])
+            elif combine == 'and':
+                final_results = result_a.intersect(result_b, wa=True, u=True)
+            elif combine == 'xor':
+                not_in_b = result_a.intersect(result_b, v=True, wa=True)
+                not_in_a = result_b.intersect(result_a, v=True, wa=True)
+                final_results = self._merge_regulators([not_in_b, not_in_a])
+            elif combine == 'not':
+                final_results = result_a.intersect(result_b, v=True, wa=True)
 
-def _get_genome_bedtool(genome_name, region, datadir=None, genes=None):
-    """get the bedtool object for a genome depending on the name and the region"""
-    genome = utils.get_genome_by_name(genome_name)
-    if region == "any":
-        filename = path.join(genome, 'all.gff')
-    elif region == "CDS":
-        filename = path.join(genome, 'cds.gff')
-    elif region == "3prime":
-        filename = path.join(genome, '3_utr.gff')
-    elif region == "5prime":
-        filename = path.join(genome, '5_utr.gff')
-    elif region == "intron":
-        filename = path.join(genome, 'intron.gff')
-    elif region == "intergenic":
-        filename = path.join(genome, 'intergenic.gff')
-    else:
-        raise ValueError("Invalid region: %r" % region)
-
-    if genes is None or 'all' in genes:
-        return BedTool(filename)
-    else:
-        return BedTool(filename).filter(lambda x: x.name in genes).saveas()
-
-def _get_regulator_bedtool(regulator_name, datadir=None):
-    """get the bedtool object for a regulator"""
-    def filter_func(rec, name):
-        if '_' in name:
-            filter_name = '_'.join(name.split('_')[1:])
         else:
-            filter_name = name
-        res = (filter_name + '*' in rec.name) or (filter_name == rec.name)
-        return res
+            final_results = result_a
 
-    if os.sep in regulator_name or '_all' in regulator_name:
-        bt = BedTool('%s.bed' % utils.get_regulator_by_name(regulator_name))
-    else:
-        bt = BedTool('%s.bed' % utils.get_regulator_by_name(regulator_name)).filter(filter_func, regulator_name).saveas()
+        regulators_a.extend(regulators_b)
+        regulator = self._merge_regulators(regulators_a)
+        final_results = final_results.intersect(regulator, wa=True, wb=True)
+        return final_results
 
-    if len(bt) > 0 and len(bt[0].fields) > 6:
-        bt = bt.bed6().saveas()
+    def _merge_regulators(self, regulators):
+        """Merge a list of regulators using BedTool.cat"""
+        regulator = regulators[0]
+        for i in range(1, len(regulators)):
+            logging.debug('merging regulator %r' % regulators[i])
+            regulator = regulator.cat(regulators[i], postmerge=False)
 
-    return bt
+        return regulator
+
+    def _add_slop(self, feature, genome_name, slop):
+        """Add specified slop before and after a regulator"""
+        return feature.slop(g=self._get_genome_chromfile(genome_name), b=slop)
+
+    def _get_genome_chromfile(self, genome_name):
+        """Get the path to the .genome file listing chromosome sizes"""
+        return path.join(self.utils.get_genome_by_name(genome_name),
+                         "{}.genome".format(genome_name))
+
+
+    def _get_genome_bedtool(self, genome_name, region, genes=None):
+        """get the bedtool object for a genome depending on the name and the region"""
+        genome = self.utils.get_genome_by_name(genome_name)
+        if region == "any":
+            filename = path.join(genome, 'all.gff')
+        elif region == "CDS":
+            filename = path.join(genome, 'cds.gff')
+        elif region == "3prime":
+            filename = path.join(genome, '3_utr.gff')
+        elif region == "5prime":
+            filename = path.join(genome, '5_utr.gff')
+        elif region == "intron":
+            filename = path.join(genome, 'intron.gff')
+        elif region == "intergenic":
+            filename = path.join(genome, 'intergenic.gff')
+        else:
+            raise ValueError("Invalid region: %r" % region)
+
+        if genes is None or 'all' in genes:
+            return BedTool(filename)
+        else:
+            return BedTool(filename).filter(lambda x: x.name in genes).saveas()
+
+    def _get_regulator_bedtool(self, regulator_name):
+        """get the bedtool object for a regulator"""
+        def filter_func(rec, name):
+            if '_' in name:
+                filter_name = '_'.join(name.split('_')[1:])
+            else:
+                filter_name = name
+            res = (filter_name + '*' in rec.name) or (filter_name == rec.name)
+            return res
+
+        if os.sep in regulator_name or '_all' in regulator_name:
+            bt = BedTool('%s.bed' % self.utils.get_regulator_by_name(regulator_name))
+        else:
+            bt = BedTool('%s.bed' % self.utils.get_regulator_by_name(regulator_name)).filter(filter_func, regulator_name).saveas()
+
+        if len(bt) > 0 and len(bt[0].fields) > 6:
+            bt = bt.bed6().saveas()
+
+        return bt
--- test/test_run.py
+++ test/test_run.py
@@ -5,13 +5,12 @@ from os import path
 from argparse import Namespace
 from pybedtools import BedTool
 
-import dorina
 from dorina import config
-from dorina import utils
 from dorina import run
 
 datadir = path.join(path.dirname(path.abspath(__file__)), 'data')
-utils = dorina.utils.DorinaUtils(datadir)
+run = run.Dorina(datadir)
+utils = run.utils
 
 class TestAnalyseWithoutOptions(unittest.TestCase):
     def setUp(self):
@@ -22,30 +21,30 @@ class TestAnalyseWithoutOptions(unittest.TestCase):
         bed_str = """chr1   doRiNA2 gene    1   1000    .   +   .   ID=gene01.01    chr1    250 260 PARCLIP#scifi*scifi_cds 5   +
         chr1    doRiNA2 gene    2001    3000    .   +   .   ID=gene01.02    chr1    2350    2360    PARCLIP#scifi*scifi_intron  5   +"""
         expected = BedTool(bed_str, from_string=True)
-        got = run.analyse('hg19', set_a=['PARCLIP_scifi'], match_a='any', region_a='any', datadir=datadir)
+        got = run.analyse('hg19', set_a=['PARCLIP_scifi'], match_a='any', region_a='any')
         self.assertEqual(expected, got)
 
-        got = run.analyse('hg19', set_a=['PARCLIP_scifi'], datadir=datadir)
+        got = run.analyse('hg19', set_a=['PARCLIP_scifi'])
         self.assertEqual(expected, got)
 
     def test_analyse_CDS_seta_single(self):
         """Test run.analyse() on CDS regions with a single regulator"""
         bed_str = """chr1   doRiNA2 CDS 201 300 .   +   0   ID=gene01.01    chr1    250 260 PARCLIP#scifi*scifi_cds 5   +"""
         expected = BedTool(bed_str, from_string=True)
-        got = run.analyse('hg19', set_a=['PARCLIP_scifi'], match_a='any', region_a='CDS', datadir=datadir)
+        got = run.analyse('hg19', set_a=['PARCLIP_scifi'], match_a='any', region_a='CDS')
         self.assertEqual(expected, got)
 
-        got = run.analyse('hg19', set_a=['PARCLIP_scifi'], region_a='CDS', datadir=datadir)
+        got = run.analyse('hg19', set_a=['PARCLIP_scifi'], region_a='CDS')
         self.assertEqual(expected, got)
 
     def test_analyse_intergenic_seta_single(self):
         """Test run.analyse() on intergenic regions with a single regulator"""
         bed_str = """chr1   doRiNA2 intergenic  1001    2000    .   .   .   ID=intergenic01.01  chr1    1250    1260    PARCLIP#scifi*scifi_intergenic  5   ."""
         expected = BedTool(bed_str, from_string=True)
-        got = run.analyse('hg19', set_a=['PARCLIP_scifi'], match_a='any', region_a='intergenic', datadir=datadir)
+        got = run.analyse('hg19', set_a=['PARCLIP_scifi'], match_a='any', region_a='intergenic')
         self.assertEqual(expected, got)
 
-        got = run.analyse('hg19', set_a=['PARCLIP_scifi'], region_a='intergenic', datadir=datadir)
+        got = run.analyse('hg19', set_a=['PARCLIP_scifi'], region_a='intergenic')
         self.assertEqual(expected, got)
 
     def test_analyse_all_regions_seta_any(self):
@@ -53,10 +52,10 @@ class TestAnalyseWithoutOptions(unittest.TestCase):
         bed_str = """chr1   doRiNA2 gene    1   1000    .   +   .   ID=gene01.01    chr1    255 265 PICTAR#fake01*fake01_cds    5   +
         chr1    doRiNA2 gene    2001    3000    .   +   .   ID=gene01.02    chr1    2450    2460    PICTAR#fake02*fake02_intron 5   +"""
         expected = BedTool(bed_str, from_string=True)
-        got = run.analyse('hg19', set_a=['PICTAR_fake01', 'PICTAR_fake02'], match_a='any', region_a='any', datadir=datadir)
+        got = run.analyse('hg19', set_a=['PICTAR_fake01', 'PICTAR_fake02'], match_a='any', region_a='any')
         self.assertMultiLineEqual(str(expected), str(got))
 
-        got = run.analyse('hg19', set_a=['PICTAR_fake01', 'PICTAR_fake02'], datadir=datadir)
+        got = run.analyse('hg19', set_a=['PICTAR_fake01', 'PICTAR_fake02'])
         self.assertEqual(expected, got)
 
     def test_analyse_all_regions_seta_all(self):
@@ -64,10 +63,10 @@ class TestAnalyseWithoutOptions(unittest.TestCase):
         bed_str = """chr1   doRiNA2 gene    1   1000    .   +   .   ID=gene01.01    chr1    250 260 PARCLIP#scifi*scifi_cds 5   +
         chr1    doRiNA2 gene    1   1000    .   +   .   ID=gene01.01    chr1    255 265 PICTAR#fake01*fake01_cds    5   +"""
         expected = BedTool(bed_str, from_string=True)
-        got = run.analyse('hg19', set_a=['PARCLIP_scifi', 'PICTAR_fake01'], match_a='all', region_a='any', datadir=datadir)
+        got = run.analyse('hg19', set_a=['PARCLIP_scifi', 'PICTAR_fake01'], match_a='all', region_a='any')
         self.assertMultiLineEqual(str(expected), str(got))
 
-        got = run.analyse('hg19', set_a=['PARCLIP_scifi', 'PICTAR_fake01'], match_a='all', datadir=datadir)
+        got = run.analyse('hg19', set_a=['PARCLIP_scifi', 'PICTAR_fake01'], match_a='all')
         self.assertEqual(expected, got)
 
     def test_analyse_all_regions_seta_and_setb(self):
@@ -77,7 +76,7 @@ class TestAnalyseWithoutOptions(unittest.TestCase):
         expected = BedTool(bed_str, from_string=True)
         got = run.analyse('hg19', set_a=['PARCLIP_scifi'], match_a='any', region_a='any',
                           set_b=['PICTAR_fake01'], match_b='any', region_b='any',
-                          combine='and', datadir=datadir)
+                          combine='and')
         self.assertMultiLineEqual(str(expected), str(got))
 
     def test_analyse_all_regions_seta_or_setb(self):
@@ -90,7 +89,7 @@ class TestAnalyseWithoutOptions(unittest.TestCase):
         expected = BedTool(bed_str, from_string=True)
         got = run.analyse('hg19', set_a=['PARCLIP_scifi'], match_a='any', region_a='any',
                           set_b=['PICTAR_fake01'], match_b='any', region_b='any',
-                          combine='or', datadir=datadir)
+                          combine='or')
         self.assertMultiLineEqual(str(expected), str(got))
 
     def test_analyse_all_regions_seta_xor_setb(self):
@@ -99,7 +98,7 @@ class TestAnalyseWithoutOptions(unittest.TestCase):
         expected = BedTool(bed_str, from_string=True)
         got = run.analyse('hg19', set_a=['PARCLIP_scifi'], match_a='any', region_a='any',
                           set_b=['PICTAR_fake01'], match_b='any', region_b='any',
-                          combine='xor', datadir=datadir)
+                          combine='xor')
         self.assertMultiLineEqual(str(expected), str(got))
 
     def test_analyse_all_regions_seta_not_setb(self):
@@ -108,7 +107,7 @@ class TestAnalyseWithoutOptions(unittest.TestCase):
         expected = BedTool(bed_str, from_string=True)
         got = run.analyse('hg19', set_a=['PARCLIP_scifi'], match_a='any', region_a='any',
                           set_b=['PICTAR_fake01'], match_b='any', region_b='any',
-                          combine='not', datadir=datadir)
+                          combine='not')
         self.assertMultiLineEqual(str(expected), str(got))
 
 
@@ -118,7 +117,7 @@ class TestAnalyseWithoutOptions(unittest.TestCase):
 chr1	doRiNA2	gene	250	260	.	+	.	ID=gene01.01	chr1	255	265	PICTAR#fake01*fake01_cds	5	+"""
         expected = BedTool(bed_str, from_string=True)
         got = run.analyse('hg19', set_a=['PARCLIP_scifi', 'PICTAR_fake01'], match_a='all', region_a='any',
-                          window_a=0, datadir=datadir)
+                          window_a=0)
         self.assertMultiLineEqual(str(expected), str(got))
 
 
@@ -131,7 +130,7 @@ chr1	doRiNA2	gene	1350	3360	.	+	.	ID=gene01.02	chr1	2350	2360	PARCLIP#scifi*scif
 chr1	doRiNA2	gene	1350	3360	.	+	.	ID=gene01.02	chr1	1350	1360	PICTAR#fake01*fake01_intergenic	5	."""
         expected = BedTool(bed_str, from_string=True)
         got = run.analyse('hg19', set_a=['PARCLIP_scifi', 'PICTAR_fake01'], match_a='all', region_a='any',
-                          window_a=1000, datadir=datadir)
+                          window_a=1000)
         self.assertMultiLineEqual(str(expected), str(got))
 
 
@@ -142,59 +141,59 @@ chr1	doRiNA2	gene	1350	3360	.	+	.	ID=gene01.02	chr1	1350	1360	PICTAR#fake01*fake
         chr1    2050    2660    PARCLIP#scifi*scifi_intron  5   +
 """
         expected = BedTool(slop_string, from_string=True)
-        got = run._add_slop(BedTool(run._get_regulator_bedtool('PARCLIP_scifi', datadir)),
-                            'hg19', 300, datadir)
+        got = run._add_slop(BedTool(run._get_regulator_bedtool('PARCLIP_scifi')),
+                            'hg19', 300)
         self.assertEqual(expected, got)
 
     def test_get_genome_chromfile(self):
         """Test run._get_genome_chromfile()"""
         expected = path.join(utils.get_genome_by_name('hg19'), 'hg19.genome')
-        got = run._get_genome_chromfile('hg19', datadir=datadir)
+        got = run._get_genome_chromfile('hg19')
         self.assertEqual(expected, got)
 
 
     def test_get_genome_bedtool(self):
         """Test run._get_genome_bedtool()"""
         # should raise a ValueError for an invalid region
-        self.assertRaises(ValueError, run._get_genome_bedtool, 'hg19', 'invalid', datadir)
+        self.assertRaises(ValueError, run._get_genome_bedtool, 'hg19', 'invalid')
 
         expected = BedTool(path.join(utils.get_genome_by_name('hg19'), 'all.gff'))
-        got = run._get_genome_bedtool('hg19', 'any', datadir)
+        got = run._get_genome_bedtool('hg19', 'any')
         self.assertEqual(expected, got)
 
         expected = BedTool(path.join(utils.get_genome_by_name('hg19'), 'cds.gff'))
-        got = run._get_genome_bedtool('hg19', 'CDS', datadir)
+        got = run._get_genome_bedtool('hg19', 'CDS')
         self.assertEqual(expected, got)
 
         expected = BedTool(path.join(utils.get_genome_by_name('hg19'), '3_utr.gff'))
-        got = run._get_genome_bedtool('hg19', '3prime', datadir)
+        got = run._get_genome_bedtool('hg19', '3prime')
         self.assertEqual(expected, got)
 
         expected = BedTool(path.join(utils.get_genome_by_name('hg19'), '5_utr.gff'))
-        got = run._get_genome_bedtool('hg19', '5prime', datadir)
+        got = run._get_genome_bedtool('hg19', '5prime')
         self.assertEqual(expected, got)
 
         expected = BedTool(path.join(utils.get_genome_by_name('hg19'), 'intron.gff'))
-        got = run._get_genome_bedtool('hg19', 'intron', datadir)
+        got = run._get_genome_bedtool('hg19', 'intron')
         self.assertEqual(expected, got)
 
         expected = BedTool(path.join(utils.get_genome_by_name('hg19'), 'intergenic.gff'))
-        got = run._get_genome_bedtool('hg19', 'intergenic', datadir)
+        got = run._get_genome_bedtool('hg19', 'intergenic')
         self.assertEqual(expected, got)
 
         expected = BedTool(path.join(utils.get_genome_by_name('hg19'), 'all.gff')).filter(
                 lambda x: x.name == "gene01.02").saveas()
-        got = run._get_genome_bedtool('hg19', 'any', datadir, genes=['gene01.02'])
+        got = run._get_genome_bedtool('hg19', 'any', genes=['gene01.02'])
         self.assertEqual(expected, got)
 
 
     def test_get_regulator_bedtool(self):
         """Test run._get_regulator_bedtool()"""
         expected = BedTool('%s.bed' % utils.get_regulator_by_name('PARCLIP_scifi')).bed6()
-        got = run._get_regulator_bedtool('PARCLIP_scifi', datadir)
+        got = run._get_regulator_bedtool('PARCLIP_scifi')
         self.assertEqual(expected, got)
 
         manual = path.join(datadir, 'manual.bed')
         expected = BedTool(manual).bed6()
-        got = run._get_regulator_bedtool(manual, datadir)
+        got = run._get_regulator_bedtool(manual)
         self.assertEqual(expected, got)
