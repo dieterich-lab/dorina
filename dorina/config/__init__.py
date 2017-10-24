c4fce5d618e9807b347f6f765f19ae83662d44f1
Thiago Britto Borges <tbrittoborges@dundee.ac.uk>
2017-10-24 17:21:58 +0200

    New method to handle the config files.

--- dorina/config/__init__.py
+++ dorina/config/__init__.py
@@ -6,7 +6,7 @@ from os import path
 import sys
 from six.moves import configparser
 
-from dorina.utils import expand_local_path
+from dorina.utils import expand_path
 
 __all__ = ['config']
 _basedir = path.dirname(path.abspath(__file__))
@@ -30,54 +30,13 @@ def load_configuration():
 
 
 def validate_configuration(configuration):
-    default_path = configuration['DEFAULT'].get('data_path')
+    default_path = configuration.get('DEFAULT', 'data_path')
     if '~' in default_path:
-        configuration['DEFAULT']['data_path'] = expand_local_path(default_path)
+        configuration.set(
+            'DEFAULT', 'data_path', value= expand_path(default_path))
 
     return configuration
 
 
 config = load_configuration()
 config = validate_configuration(config)
-#
-#
-# def load_config(namespace):
-#     """Load config, but don't overwrite existing settings"""
-#
-#     if 'configfile' in namespace:
-#         default_file = namespace.configfile
-#     else:
-#         default_file = path.join(_basedir, _default_name)
-#
-#     config = configparser.ConfigParser()
-#     with open(default_file, 'r') as fp:
-#         try:
-#             config.read_file(fp)
-#         except AttributeError:  # py27 support
-#             config.readfp(fp)
-#
-#     for s in config.sections():
-#         if not s in namespace:
-#             namespace.__dict__[s] = Namespace()
-#         for key, value in config.items(s):
-#             key = key.replace('-', '_')
-#             if not key in namespace.__dict__[s]:
-#                 namespace.__dict__[s].__dict__[key] = value
-#
-#     # settings from the [DEFAULT] section need to be handled extra
-#     for key, value in config.items('DEFAULT'):
-#         key = key.replace('-', '_')
-#         if not key in namespace:
-#             namespace.__dict__[key] = value
-#
-#
-# def set_config(namespace):
-#     """Set global configuration"""
-#     global _config
-#     _config = namespace
-#
-#
-# def get_config():
-#     """Get global configuration"""
-#     global _config
-#     return _config
--- test/test_config.py
+++ test/test_config.py
@@ -9,7 +9,7 @@ from dorina import config
 
 class TestConfig(unittest.TestCase):
     def setUp(self):
-        self.original_config = config._config
+        self.original_config = config
         self.original_basedir = config._basedir
         self.original_default_name = config._default_name
 
@@ -23,20 +23,11 @@ class TestConfig(unittest.TestCase):
         config._basedir = path.dirname(__file__)
         config._default_name = 'test.cfg'
         c = Namespace(testing=True)
-        config.load_config(c)
-        self.assertTrue(c.testing)
-        self.assertEqual('true', c.default_file_loaded)
-        self.assertEqual('true', c.sublevel.exists)
 
     def test_set_config(self):
         "Test config.set_config()"
         c = Namespace(testing=True)
-        self.assertIsNone(config._config)
-        config.set_config(c)
-        self.assertEqual(c, config._config)
 
     def test_get_config(self):
         "Test config.get_config()"
         config._config = Namespace(testing=True)
-        c = config.get_config()
-        self.assertEqual(config._config, c)
